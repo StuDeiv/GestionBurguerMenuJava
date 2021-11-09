68b2570422fbb71c9e5582301a3dfa732fe021b0
dsantosc04 <dsantosc04@infolab02-w27.instituto.extremadura.es>
2021-11-09 19:18:07 +0100

    Initial commit

---
+++ GestionBurguerMenuJavaSwing/src/gestionburguermenujavaswing/GestionBurguerMenuJavaSwing.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestionburguermenujavaswing;
+
+/**
+ *
+ * @author dsantosc04
+ */
+public class GestionBurguerMenuJavaSwing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
